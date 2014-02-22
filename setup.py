96f0d8e2ec20bf38a35eac44c4d2c82e6704882e
Stuart Hickinbottom <stuart@hickinbottom.com>
2014-02-22 21:33:52 +0000

    Added reference to Gentoo package
    This addresses issue#22 by referencing a Gentoo package for the
    utility.

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import distribute_setup
+    distribute_setup.use_setuptools()
+    from setuptools import setup
+
+with open('README.txt') as file:
+    long_description = file.read()
+
+setup(
+    name="galileo",
+    version='0.4dev',
+    description="Utility to securely synchronize a Fitbit tracker with the"
+                " Fitbit server",
+    long_description=long_description,
+    author="Benoît Allard",
+    author_email="benoit.allard@gmx.de",
+    url="https://bitbucket.org/benallard/galileo",
+    platforms='any',
+    keywords=['fitbit', 'synchronize', 'health', 'tracker'],
+    license="LGPL",
+    install_requires=[
+        "requests",
+        "pyusb>=1a"],  # version 1a doesn't exists, but is smaller than 1.0.0a2
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or'
+        ' later (LGPLv3+)',
+        'Environment :: Console',
+        'Topic :: Utilities',
+        'Topic :: Internet',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'galileo = galileo.main:main'
+        ],
+    },
+)
